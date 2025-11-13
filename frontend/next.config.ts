48859247083aead27d1bd7b2e7ad3bfa408d167f
Tiara S Dewi <trsrdw@gmail.com>
2025-11-13 22:26:45 +0700

    add more remote patterns

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -12,10 +12,22 @@ const nextConfig: NextConfig = {
       {
         protocol: "https",
         hostname: `${process.env.NEXT_PUBLIC_AWS_BUCKET}.s3.${process.env.NEXT_PUBLIC_AWS_REGION}.amazonaws.com`,
+        pathname: "/**",
       },
       {
         protocol: "https",
         hostname: "legendary-delight-f00eac2500.strapiapp.com",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "legendary-delight-f00eac2500.media.strapiapp.com",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "blog.tiarasdewi.com",
+        pathname: "/**",
       },
     ]
   },
